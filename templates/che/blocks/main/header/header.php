ac09afefeb22b011b7ac89611e720bd2bc90bb23
dmitriy <chevil92@gmail.com>
2014-10-04 12:30:06 +0300

    пшер

--- templates/che/blocks/main/header/header.php
+++ templates/che/blocks/main/header/header.php
@@ -1,10 +1,12 @@
 <div class="header">
-    <div class="block_2 languages">
-        <jdoc:include type="modules" name="languages"/>
-    </div>
+
     <div class="block_6 logo">
         <jdoc:include type="modules" name="logo"/>
     </div>
+    <div class="block_2 languages">
+        <jdoc:include type="modules" name="languages"/>
+    </div>
+
     <div class="block_2 authorization">
         <jdoc:include type="modules" name="authorization"/>
     </div>
