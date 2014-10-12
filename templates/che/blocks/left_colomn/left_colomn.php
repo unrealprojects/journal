fa584b1f3a552e4ae587d10b5acc34a1e6eb5b44
a <a>
2014-10-13 02:36:24 +0300

    asd

--- templates/che/blocks/left_colomn/left_colomn.php
+++
@@ -1,6 +0,0 @@
-<aside class="Page-Sidebar">
-    <nav class="Site-Navigation">
-        <jdoc:include type="modules" name="left_nav"/>
-    </nav>
-    <button class="Feed-Subscription">Подписка на сайт</button>
-</aside>
