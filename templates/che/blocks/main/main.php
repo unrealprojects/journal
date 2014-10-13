aef56f78ed030a8ae8f2e51d47e123d5e9ab515a
a <a>
2014-10-13 03:37:13 +0300

    asd

--- templates/che/blocks/main/main.php
+++ templates/che/blocks/main/main.php
@@ -1,7 +1,41 @@
-<div class="main">
+<!-- Site Header -->
+<header class="Site-Header">
+    <div class="Sidebar-Toggle"></div>
+    <div class="Site-Logo">
+        <h2>
+            Фармакогенетика<span>Фармакогеномика</span>
+        </h2>
+    </div>
+    <div class="Toolbar">
+        <div class="Languages">
+            <jdoc:include type="modules" name="languages"/>
+        </div>
+
+        <nav class="Authorization">
+            <ul>
+                <li><a href="#">Вход</a></li>
+                <li><a class="Button-Like" href="#">Регистрация</a></li>
+            </ul>
+            <jdoc:include type="modules" name="authorization"/>
+        </nav>
+    </div>
+</header>
+
+<!-- Site Navigation: Primary -->
+<section class="Site-Navigation Primary Grid Merge">
+    <nav class="Node-XS-9">
+        <jdoc:include type="modules" name="top_nav"/>
+    </nav>
+
+    <div class="Search-Form Node-XS-3">
+        <jdoc:include type="modules" name="search"/>
+    </div>
+</section>
+<main>
+
     <?php
-        include $parent_dir.'/main/header/header.php';
-        include $parent_dir.'/main/nav/nav.php';
-        include $parent_dir.'/main/page/page.php';
+    include $parent_dir.'/main/page/page.php';
     ?>
-</div>
+
+</main>
+
--- templates/che/blocks/main/nav/nav.php
+++
@@ -1,9 +0,0 @@
-<section class="top_nav Grid Merge">
-    <nav class="Nav-Inline Node-XS-9">
-        <jdoc:include type="modules" name="top_nav"/>
-    </nav>
-
-    <div class="Search-Form Node-XS-3">
-        <jdoc:include type="modules" name="search"/>
-    </div>
-</section>
