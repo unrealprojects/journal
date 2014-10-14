45417e92883cb2cf3bf1ea657479e842425cfb9c
a <a>
2014-10-14 20:12:06 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	templates/journal/style.css.map

--- templates/journal/blocks/main/page/page.php
+++ templates/journal/blocks/main/page/page.php
@@ -23,14 +23,14 @@
                         <div class="Top-Five-Header">
                         <h3>Топ 5</h3>
                         <div class="Dropdown Collapsed">
-                            <div class="Dropdown-Title">За неделю<span class="Dropdown-Toggle fa fa-close"></span></div>
-                            <ul class="Dropdown-Content" style="display: block;">
+                            <div class="Dropdown-Title">За неделю<span class="Dropdown-Toggle"></span></div>
+                            <ul class="Dropdown-Content">
                                 <li class="Icon"><a href="#">За все время</a></li>
                                 <li class="Icon"><a href="#">За месяц</a></li>
                                 <li class="Icon"><a href="#">За неделю</a></li>
                             </ul>
                         </div>
-                        </div>
+                    </div>
                         <div class="Popular">
                             <jdoc:include type="modules" name="popular" />
                         </div>
@@ -69,13 +69,16 @@
 
                 <?php  if( $this->countModules('popular_right') ) : ?>
                     <div class="Top-Five">
-                        <div class="Dropdown Expanded">
-                            <div class="Dropdown-Title">Топ 5 за неделю<span class="Dropdown-Toggle"></span></div>
-                            <ul class="Dropdown-Content" style="display: block;">
-                                <li class="Icon"><a href="#">За все время</a></li>
-                                <li class="Icon"><a href="#">За месяц</a></li>
-                                <li class="Icon"><a href="#">За неделю</a></li>
-                            </ul>
+                        <div class="Top-Five-Header">
+                            <h3>Топ 5</h3>
+                            <div class="Dropdown Collapsed">
+                                <div class="Dropdown-Title">За неделю<span class="Dropdown-Toggle"></span></div>
+                                <ul class="Dropdown-Content">
+                                    <li class="Icon"><a href="#">За все время</a></li>
+                                    <li class="Icon"><a href="#">За месяц</a></li>
+                                    <li class="Icon"><a href="#">За неделю</a></li>
+                                </ul>
+                            </div>
                         </div>
                         <div class="Popular">
                             <jdoc:include type="modules" name="popular_right" />
