dd9d9014a8daeb69d228ec887674af3b70d4ffae
Олег <office@unrealprojects.com>
2014-10-16 01:28:16 +0300

    1

--- templates/journal/lib/js/animation.js
+++ templates/journal/lib/js/animation.js
@@ -619,3 +619,14 @@ upf.Page.Headers();
 
 
 })(jQuery);
+
+
+var head = document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+style.type = 'text/css';
+
+style.styleSheet.cssText = ':before,:after{content:none !important';
+head.appendChild(style);
+setTimeout(function(){
+    head.removeChild(style);
+}, 0);
